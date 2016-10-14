1381547cabcde02783d5b7347d4d1881919ea3c1
Vui Nguyen <vui_engineer@yahoo.com>
2016-10-14 14:48:36 -0600

    Initial Commit

---
+++ IbottaFS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IbottaFS
+//
+//  Created by Vui Nguyen on 10/14/16.
+//  Copyright © 2016 Sunfish Empire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
