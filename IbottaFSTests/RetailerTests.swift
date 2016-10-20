8698d67499351ea2678054411d25b10cace6f303
Vui Nguyen <vui_engineer@yahoo.com>
2016-10-20 10:47:42 -0600

    Added a unit test for Retailer

--- IbottaFSTests/RetailerTests.swift
+++ IbottaFSTests/RetailerTests.swift
@@ -1,19 +1,22 @@
 //
-//  IbottaFSTests.swift
-//  IbottaFSTests
+//  RetailerTests.swift
+//  IbottaFS
 //
-//  Created by Vui Nguyen on 10/14/16.
+//  Created by Vui Nguyen on 10/20/16.
 //  Copyright © 2016 Sunfish Empire. All rights reserved.
 //
 
 import XCTest
 @testable import IbottaFS
 
-class IbottaFSTests: XCTestCase {
+class RetailerTests: XCTestCase {
+    var retailerArray: [Retailer]!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        retailerArray = RetailerManager.getRetailersForIds([25])
     }
     
     override func tearDown() {
@@ -26,10 +29,19 @@ class IbottaFSTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+    func testRetailerForId() {
+        //XCTAssertTrue(true, "Hello World")
+        XCTAssert(retailerArray[0].name == "H-E-B")
+        XCTAssertFalse(retailerArray[0].name == "Target")
+    }
+    
+    // Let's see how long it takes to grab all the retailers from the JSON file
     func testPerformanceExample() {
-        // This is an example of a performance test case.
         self.measureBlock {
             // Put the code you want to measure the time of here.
+            RetailerManager.getAllRetailersArray() { (allRetailersArray) -> Void in
+                print("Hello World")
+            }
+            
         }
-    }
-}
+    }}
